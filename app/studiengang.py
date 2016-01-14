6bae5bb809f1a5db21c8092d398a5ac0c930ce58
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-01-14 18:26:09 +0100

    Fixi fix

--- app/studiengang.py
+++ app/studiengang.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import json 
+import json
 import cherrypy
 
 from app import database, template
@@ -26,83 +26,82 @@ id > 0 !
 
 """
 
+
 class Studiengang_cl(object):
-	eposed = True
+    exposed = True
 
-	def __init__(self):
-		self.db_o = database.Database_cl()
+    def __init__(self):
+        self.db_o = database.Database_cl()
 
-	def GET(self):
-		retVal_o = {
-	        'data': 1
-	    }
-		if id == None:
-			# Anforderung der Liste
-			retVal_o['data'] = self.getList_p()
-		else:
-			 # Anforderung eines Dokuments
-			retVal_o['data'] = self.getForm_p(id)
+    def GET(self, id = None):
+        retVal_o = {
+            'data': None
+        }
+        if id is None:
+            # Anforderung der Liste
+            retVal_o['data'] = self.getList_p()
+        else:
+            # Anforderung eines Dokuments
+            retVal_o['data'] = self.getForm_p(id)
 
-		if retVal_o['data'] == None:
-		 	cherrypy.response.status = 404
+        if retVal_o['data'] is None:
+            cherrypy.response.status = 404
 
-		return json.dumps(retVal_o)
+        return json.dumps(retVal_o)
 
+    def PUT(self, **data_opl):
+        retVal_o = {
+            'id': None
+        }
+        # data_opl: Dictionary mit den gelieferten key-value-Paaren
+        # hier müsste man prüfen, ob die Daten korrekt vorliegen!
 
+        data_o = {
+            'studiengang': data_opl["studiengang_s"],
+            'kurzBezeichnung': data_opl["kurzBezeichnung_s"],
+            'semesterAnzahl': data_opl["semesterAnzahl_s"]
+        }
+        # Create-Operation
+        id_s = self.db_o.create_px(data_o)
+        retVal_o['id'] = id_s
+        if id_s is None:
+            cherrypy.response.status = 409
 
-	def PUT(self, **data_opl):
-		retVal_o = {
-		 	'id': None
-		}
-		# data_opl: Dictionary mit den gelieferten key-value-Paaren
-		# hier müsste man prüfen, ob die Daten korrekt vorliegen!
+        return json.dumps(retVal_o)
 
-		data_o = {
-			'studiengang': data_opl["studiengang_s"],
-			'kurzBezeichnung': data_opl["kurzBezeichnung_s"],
-			'semesterAnzahl': data_opl["semesterAnzahl_s"]
-		}
-		# Create-Operation
-		id_s = self.db_o.create_px(data_o)
-		retVal_o['id'] = id_s
-		if id_s == None:
-		 	cherrypy.response.status = 409
-		 
-		return json.dumps(retVal_o)
+    def POST(self, id, **data_opl):
+        retVal_o = {
+            'id': None
+        }
 
-	def POST(self, id, **data_opl):
-		retVal_o = {
-			'id': None
-		}
+        id_s = data_opl["id_s"]
+        data_o = {
+            'studiengang': data_opl["studiengang_s"],
+            'kurzBezeichnung': data_opl["kurzBezeichnung_s"],
+            'semesterAnzahl': data_opl["semesterAnzahl_s"]
+        }
+        retVal_o['id_s'] = id_s
+        if self.db_o.update_px(id_s, data_o):
+            pass
+        else:
+            cherrypy.response.status = 404
+        return json.dumps(retVal_o)
 
-		id_s = data_opl["id_s"]
-		data_o = {
-			'studiengang': data_opl["studiengang_s"],
-			'kurzBezeichnung': data_opl["kurzBezeichnung_s"],
-			'semesterAnzahl': data_opl["semesterAnzahl_s"]
-		}
-		retVal_o['id_s'] = id_s
-		if self.db_o.update_px(id_s, data_o):
-			pass
-		else:
-			cherrypy.response.status = 404
-		return json.dumps(retVal_o)
+    def DELETE(self, id):
+        # Studiengang mit der Id :studiengang-id löschen
+        # fehlt: Bestehende Lehrveranstaltungen und deren Zuordnung zu Modulen
+        # werden entfernt
+        retVal_o = {
+            'id': id
+        }
 
-	def DELETE(self, id):
-		#Studiengang mit der Id :studiengang-id löschen
-		#fehlt: Bestehende Lehrveranstaltungen und deren Zuordnung zu Modulen
-		#werden entfernt
-		retVal_o = {
-        	'id': id
-      	}
+        if self.db_o.delete_px(id):
+            pass
+        else:
+            cherrypy.response.status = 404
 
-		if self.db_o.delete_px(id):
-			pass
-		else:
-			cherrypy.response.status = 404
+        return json.dumps(retVal_o)
 
-		return json.dumps(retVal_o)
-
-	def getList_p(self):
-		data_o = self.db_o.read_px()
-		return data_o
+    def getList_p(self):
+        data_o = self.db_o.read_px()
+        return data_o
